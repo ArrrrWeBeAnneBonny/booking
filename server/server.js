58c992cf29eff1163e44971c33571f94d74f5a8c
Hannah Manfredi <hmanfredi@gmail.com>
2021-05-20 20:59:13 -0700

    efactor main /booking endpt so it pings the Overview API on to initialize my Service with data and handle error

--- server/server.js
+++ server/server.js
@@ -1,26 +1,97 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 const db = require('../database/index.js');
 const app = express();
+
+//allow my service to accept every other proxy
+const origins = ['http://localhost:2000',
+'http://localhost:5500',
+'http://localhost:3000'];
+  // add michael's proxy port numb when avail.
+
+app.use(cors());
+app.use(cors({ origin: origins }));
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//root init route
 app.get('/booking', async (req, res) => {
   let campId = parseInt(req.query.campId);
-  res.setHeader('Content-Type', 'application/json');
+  let name = '';
+  let inventory = [];
   await db.Booking.find({campId: campId})
   .then((site) => {
     let siteObj = site[0];
-    console.log('siteObj: ', siteObj);
-    res.status(200).send(siteObj);
+    inventory = siteObj.booked;
   })
   .catch((err) => {
     throw err;
   });
+  await axios.get('http://localhost:3003/overview', { params: { campId: campId } })
+    .then((response) => {
+      name = response.data.name;
+    })
+    .then(() => {
+      axios.get('http://localhost:3003/overview/pricing', { params: { campId: campId } })
+        .then((response) => {
+          let init = {};
+          init.name = name;
+          init.inventory = inventory;
+          init.average_price_per_night = response.data.averagePricePerNight;
+          init.months_out_for_booking = response.data.monthsOutForBooking;
+          init.weeknight_discount = response.data.weeknightDiscount;
+          init.instant_book = response.data.instantBook;
+          init.cleaning_fee = response.data.cleaningFee;
+          //init.max_guests = data.max_guests; ask turbo to add
+          console.log('init: ', init);
+          res.status(200).send(init);
+        })
+        .catch((err) => {
+          res.status(201).send(err);
+        });
+    })
+    .catch((err) => {
+      db.Booking.find({campId: 0})
+        .then((site) => {
+          let siteObj = site[0];
+          res.status(200).send(siteObj);
+        })
+        .catch((err) => {
+          res.status(201).send(err);
+        });
+    });
 });
+
+app.get('/booking/?campId=/book', async (req, res) => {
+  //define current month
+  //get booked inventory to populate front end
+  //checkIn = Timestamp (ISO 8601)
+  //checkOut = Timestamp (ISO 8601)
+});
+
+app.get('/booking/?campId=&check_in_date=&check_out_date=/bookingTotal', async (req, res) => {
+  // camp_id
+  // Number
+  // check_in_date
+  // Timestamp (ISO 8601)
+  // check_out_date
+  // Timestamp (ISO 8601)
+  // number_nights
+  // Number
+  // total
+  // Number
+  // subTotal
+  // Number
+  // cleaning_fee
+  // Number
+  // weeknight_discount
+  // Number
+});
+
 
 module.exports = app;
 
@@ -40,4 +111,3 @@ module.exports = app;
 // *as part of click event, move over to checkOut
 // *smoke and mirrors
 
-//add call lodinginfo API overview
