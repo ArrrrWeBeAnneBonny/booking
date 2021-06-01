e7aee5d4ddab93305d9c85fe37492bfa7fc5aa2d
Hannah Manfredi <hmanfredi@gmail.com>
2021-05-31 20:20:29 -0700

    Testing out my new 3-way webpack config setup and it works. Luv my mssg that tells me which process.env mode I am in!

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,3 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//   entry: {
-//     app: './src/index.js',
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       title: 'Production',
-//     }),
-//   ],
-//   output: {
-//     filename: '[name].bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//     clean: true,
-//   },
-// };
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
