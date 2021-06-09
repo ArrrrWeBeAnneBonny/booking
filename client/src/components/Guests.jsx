4b2c1960f127770182b29ed13efac179812ad886
Hannah Manfredi <hmanfredi@gmail.com>
2021-06-08 20:47:00 -0700

    css: make things smaller

--- client/src/components/Guests.jsx
+++ client/src/components/Guests.jsx
@@ -3,20 +3,30 @@ import React from 'react';
 class Guests extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      guest_numb: 2
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    let guest = e.target.value;
+    console.log('guest: ', guest);
+    this.setState({
+      guest_numb: guest
+    });
   }
 
   render() {
     return (
       <div className="row">
-        <div className="col-xs-12 guests select">
+        <div className="col-xs-12 guests hc-select">
           <div className="label">Guests</div>
-          <span className="value">2 guests</span>
-          <select name="group_size" id="group_size" className="invisible-input">
+          <br></br>
+          <span className="value guests">{this.state.guest_numb} guests</span>
+          <select name="group_size" onChange={this.handleSubmit}>
+            <option value="1">1 guest</option>
             <option value="2">2 guests</option>
             <option value="3">3 guests</option>
             <option value="4">4 guests</option>
