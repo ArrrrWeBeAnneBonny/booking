2785daf8afb15a1011164e4f1135af8a51ae7c70
Hannah Manfredi <hmanfredi@gmail.com>
2021-06-10 20:06:08 -0700

    Remove aws from config.js

--- config.js
+++ config.js
@@ -6,12 +6,5 @@ module.exports = {
   development: {
     booking: "http://localhost:3002",
     overview: "http://localhost:3003/overview.js"
-  },
-  aws: {
-    accessKeyId: "AKIAUQ4Y2QZOECW3EBG5",
-    secretAccessKey: "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"
-  },
-  view: {
-    booking_service: "http://ec2-13-57-19-184.us-west-1.compute.amazonaws.com:3002"
   }
 }
